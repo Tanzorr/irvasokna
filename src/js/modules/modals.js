150afc4ca66d55cf213623949a37eee08888a72f
alexx <alexx1984@ukr.net>
2021-08-27 14:49:37 +0300

    final commit:

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -5,7 +5,8 @@ const modals =()=>{
         const  triggers = document.querySelectorAll(triggerSelector),
             modal = document.querySelector(modalSelector),
             close = document.querySelector(closeSelector),
-            windows = document.querySelectorAll('[data-modal]');
+            windows = document.querySelectorAll('[data-modal]'),
+            scroll = calcScroll();
 
         triggers.forEach((items)=>{
             items.addEventListener('click', (e)=>{
@@ -18,6 +19,7 @@ const modals =()=>{
                 });
 
                 modal.style.display = "block";
+                document.body.style.marginRight = `${scroll}px`
                 //document.body.style.overflow = "hidden";
                 document.body.classList.add('modal-open');
             });
@@ -25,9 +27,11 @@ const modals =()=>{
 
         close.addEventListener('click', ()=>{
             modal.style.display = "none";
+            document.body.style.marginRight = `0px`
            // document.body.style.overflow = "";
             windows.forEach(item =>{
                 item.style.display = 'none';
+                document.body.style.marginRight = `0px`
             });
             document.body.classList.remove('modal-open');
         });
@@ -45,6 +49,21 @@ const modals =()=>{
         })
     }
 
+    function calcScroll(){
+        let div = document.createElement('div');
+
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.overflowY = 'scroll';
+        div.style.visibility = 'hidden';
+
+        document.body.appendChild(div);
+        let scrollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+
+        return scrollWidth;
+    }
+
     function showModalByTime(selector, time){
         setTimeout(()=>{
             document.querySelector(selector).style.display = 'block';
@@ -56,7 +75,7 @@ const modals =()=>{
     bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
     bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false);
     bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
-   // showModalByTime('.popup', 60000)
+    showModalByTime('.popup', 60000)
 
 }
 
